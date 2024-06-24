213956f1c04ca2e94cfd17665f0f9349b44a53f7
felixateya <mulaafelix6@gmail.com>
2024-06-25 00:09:11 +0300

    new changes

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -16,7 +16,7 @@ const HomeLayout = () => {
         <h3 className="text-2xl font-bold text-left border-b-2 h-max p-2 border-b-gray-600 text-white capitalize">
           {path === "" ? "home" : path}
         </h3>
-        <main className="w-full overflow-y-auto ">
+        <main className="w-full overflow-y-auto overflow-x-hidden ">
           <Outlet />
         </main>
       </div>
