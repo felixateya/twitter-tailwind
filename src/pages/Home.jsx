b583eb6d304c8185e0f09b32bc1dff9ef9b97a7f
felixateya <mulaafelix6@gmail.com>
2024-06-25 00:07:51 +0300

    new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,15 @@ import TweetDisplay from "../components/TweetDisplay";
 import TweetForm from "../components/TweetForm";
 
 const Home = () => {
-  const arr = [1,2, 3,4,5]
-  return <div>
-    <TweetForm/>
-    {arr.map((num)=>(
-    <TweetDisplay key={num}/>
-
-    ))}
-    
-  </div>;
+  const arr = [1, 2];
+  return (
+    <div>
+      <TweetForm />
+      {arr.map((num) => (
+        <TweetDisplay key={num} />
+      ))}
+    </div>
+  );
 };
 
 export default Home;
