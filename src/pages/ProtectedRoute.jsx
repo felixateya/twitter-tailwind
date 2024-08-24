f810c9b834adc7f962e7df88545a67109d373924
felixateya <mulaafelix6@gmail.com>
2024-08-24 23:11:27 +0300

    modify the protected route for better performance

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -2,18 +2,14 @@
 import {useEffect, useState } from 'react';
 import { useNavigate, Outlet } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-// eslint-disable-next-line no-unused-vars
+
 import { app } from '../firebase';
 import FullpageLoader from '../components/FullpageLoader';
-// import Loader from '../Components/Loader';
 const ProtectedRoute = () => {
   const [loading, setLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const auth = getAuth();
   const navigate = useNavigate()
-  // const style = {
-  //   backgroundColor: "#293A58",
-  // };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -28,7 +24,7 @@ const ProtectedRoute = () => {
     return <FullpageLoader/> // You can replace this with a loader/spinner component
   }
 
-  if (!isAuthenticated) navigate("/login");
+  if (!isAuthenticated) return navigate("/login");
 
   return <Outlet />;
 };
